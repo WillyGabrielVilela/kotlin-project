bbd6c477a7d9e65d03c7430ea317317229035061
Willy <willygabrielnoval@gmail.com>
2024-06-04 17:22:19 -0300

    First commit RN

---
+++ paginatrocadaRN/App.tsx
@@ -0,0 +1,8 @@
+import { SignIn } from './src/screens/SignIn';
+
+export default function App() {
+  return (
+    <SignIn />
+  );
+}
+
---
+++ paginatrocadaRN/src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import {Text, View } from 'react-native';
+
+import { styles } from './styles'
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá</Text>
+    </View>
+  );
+}
+
