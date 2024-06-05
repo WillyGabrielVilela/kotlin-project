1f96bbabc59ed5d9dc44f9c8415efdd94e704517
Willy <willygabrielnoval@gmail.com>
2024-06-05 15:23:58 -0300

    Feat pagina de SignIn

--- Pagina_Trocada/src/screens/SignIn/index.tsx
+++ Pagina_Trocada/src/screens/SignIn/index.tsx
@@ -1,12 +1,24 @@
-import { Image, Text, View } from 'react-native';
-import { styles } from './styles';
+import { Image } from 'react-native';
+import { Container, ImageLogo, Slogan, Title } from './styles';
 
+import { Button } from '../../components/Button';
+import backgroundImg from '../../assets/background.png'
+import logoImg from '../../../assets/icon.png'
+import googleImg from '../../assets/googlebutton.png'
+import { Container as GoogleButton } from '../../components/Button/styles';
 export function SignIn() {
-  return (
-    <View style={styles.container}>
-      <Text>Olá</Text>
-    </View>
-  );
+    return (
+        <Container source={backgroundImg}>
+            <ImageLogo source={logoImg} />
+            <Title>Pagina Trocada</Title>
+            <Slogan>
+                Faça login e troque experiências literárias com outras pessoas!
+            </Slogan>
+            <GoogleButton style={{backgroundColor: "transparent"}}>
+                <Image source={googleImg}/>
+            </GoogleButton>
+        </Container>
+    );
 }
 
 
--- Pagina_Trocada/src/screens/SignIn/styles.ts
+++ Pagina_Trocada/src/screens/SignIn/styles.ts
@@ -1,10 +1,31 @@
-import { StyleSheet } from "react-native";
+import styled from 'styled-components/native';
 
-export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+export const Container = styled.ImageBackground`
+  flex: 1;
+  justify-content: center;
+  padding: 52px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_800};
+`;
+
+export const ImageLogo = styled.Image`
+  width: 90%;
+  margin-left: 15px;
+  margin-bottom: 20px;
+  padding: 52px;
+`
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_500};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XXXL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+
+export const Slogan = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_600};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  text-align: center;
+  margin-bottom: 32px;
+`; 
