0511eb6caa868efb3b1f11e3d58981121d25bc6c
Willy <willygabrielnoval@gmail.com>
2024-06-05 14:47:09 -0300

    configuração da font Inter e Status Bar

--- Pagina_Trocada/App.tsx
+++ Pagina_Trocada/App.tsx
@@ -1,10 +1,25 @@
 import { ThemeProvider } from 'styled-components/native';
 import { SignIn } from './src/screens/SignIn';
 import theme from './src/theme';
+import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold})
+
+  if(!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+      barStyle="dark-content" 
+      backgroundColor="transparent" 
+      translucent 
+      />
       <SignIn />
 
     </ThemeProvider>
