d7a9e86c3cf9e2d2005f54f241a37371dbb666c9
Willy <willygabrielnoval@gmail.com>
2024-06-10 16:25:07 -0300

    configuração de autenticação pelo google com realm, rotas e safearea

--- Pagina_Trocada/App.tsx
+++ Pagina_Trocada/App.tsx
@@ -1,28 +1,40 @@
 import { ThemeProvider } from 'styled-components/native';
 import { SignIn } from './src/screens/SignIn';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import theme from './src/theme';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+import { AppProvider, UserProvider } from '@realm/react';
+import { REALM_APP_ID } from '@env';
+
+
 import { Loading } from './src/components/Loading';
 import { StatusBar } from 'react-native';
+import { Routes } from './src/routes';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold})
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold })
 
-  if(!fontsLoaded) {
+  if (!fontsLoaded) {
     return (
       <Loading />
     )
   }
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar 
-      barStyle="dark-content" 
-      backgroundColor="transparent" 
-      translucent 
-      />
-      <SignIn />
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <UserProvider fallback={SignIn}>
+            <Routes />
+          </UserProvider>
 
-    </ThemeProvider>
+        </SafeAreaProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
 
