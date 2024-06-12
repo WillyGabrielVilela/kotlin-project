9b02bd178aed0c97328f1fe0a271d70f5b754924
Willy <willygabrielnoval@gmail.com>
2024-06-11 22:11:46 -0300

    navegação da home

--- Pagina_Trocada/src/screens/Home/index.tsx
+++ Pagina_Trocada/src/screens/Home/index.tsx
@@ -1,10 +1,20 @@
+import { useNavigation } from '@react-navigation/native';
 import { Header } from '../../components/Header';
 import { Container } from './styles';
+import RoundedButton  from '../../components/RoundedButton';
 
 export function Home() {
+
+  const { navigate } = useNavigation();
+
+  function handleRegisterMoviment() {
+    navigate('addbook')
+  }
+
   return (
     <Container>
       <Header />
+      <RoundedButton onPress={handleRegisterMoviment} />
     </Container>
   );
 }
