ec759fd3bf659614dc991944f59a0d5659d17a6a
Willy <willygabrielnoval@gmail.com>
2024-06-13 15:42:18 -0300

    Adição Pagina de Perfil

--- Pagina_Trocada/src/screens/Profile/index.tsx
+++ Pagina_Trocada/src/screens/Profile/index.tsx
@@ -1,9 +1,34 @@
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { useApp, useUser } from '@realm/react';
+import { Button, FlatList } from 'react-native';
+import { Container, Name, Picture, SectionTitle, BookItem, BookImage, BookTitle, ButtonText } from './styles';
+import { clearStorage } from '../../storage/storage';
+import { Book } from '../../libs/schemas/BookUserModels';
 
 export function Profile() {
+  const user = useUser();
+  const app = useApp()
+
+  const [books, setBooks] = useState<Book[]>([]);
+
+
+  const handleClearStorage = async () => {
+    await clearStorage();
+    setBooks([]);
+  };
+
+  function handleLogout() {
+    app.currentUser?.logOut()
+  }
+
   return (
     <Container>
-        
+      <Picture source={{ uri: user?.profile.pictureUrl }} />
+      <Name>{user?.profile.name}</Name>
+
+        <ButtonText title="Limpar Storage" onPress={handleClearStorage}>Limpar Storage</ButtonText>
+        <ButtonText title="" onPress={handleLogout}>Deslogar</ButtonText>
+
     </Container>
   );
 }
