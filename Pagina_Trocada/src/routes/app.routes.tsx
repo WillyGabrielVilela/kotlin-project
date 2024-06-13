07cea97e42e7ead29e846add15d4e5db8dd95ac7
Willy <willygabrielnoval@gmail.com>
2024-06-13 11:00:49 -0300

    roteamento da pagina de bookdetails

--- Pagina_Trocada/src/routes/app.routes.tsx
+++ Pagina_Trocada/src/routes/app.routes.tsx
@@ -2,24 +2,36 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "../screens/Home";
 import { AddBook } from "../screens/AddBook";
 import { Profile } from "../screens/Profile";
+import { BookDetails } from "../screens/BookDetails"; // Certifique-se de importar o componente BookDetails
 
-const { Navigator, Screen } = createNativeStackNavigator()
+
+type AppStackParamList = {
+    home: undefined;
+    addbook: undefined;
+    profile: undefined;
+    book: { bookId: string };
+};
+const { Navigator, Screen } = createNativeStackNavigator<AppStackParamList>();
 
 export function AppRoutes() {
     return (
-        <Navigator screenOptions={{headerShown: false}}>
-            <Screen 
-            name="home"
-            component={Home}
+        <Navigator screenOptions={{ headerShown: false }}>
+            <Screen
+                name="home"
+                component={Home}
             />
-            <Screen 
-            name="addbook"
-            component={AddBook}
+            <Screen
+                name="addbook"
+                component={AddBook}
             />
-            <Screen 
-            name="profile"
-            component={Profile}
+            <Screen
+                name="profile"
+                component={Profile}
+            />
+            <Screen
+                name="book"
+                component={BookDetails}
             />
         </Navigator>
-    )
+    );
 }
